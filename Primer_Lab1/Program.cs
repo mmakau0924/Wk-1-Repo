52d82161cf6d1d0f1f937e736458b5771a5f50dd
MaloneMakau <info@malone-makau.com>
2023-01-27 21:09:26 +0300

    This SHould Work

---
+++ Primer_Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Primer_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            // Age Qn
+
+            Console.WriteLine("Enter Your Age:");
+            string Age = Console.ReadLine();
+            int AgeNo = Convert.ToInt32(Age);
+
+            if (AgeNo >= 18)
+                {
+                Console.WriteLine("Adult");
+                
+            }
+            else
+            {
+                Console.WriteLine("Not Adult");
+            }
+            
+        }
+    }
+}
